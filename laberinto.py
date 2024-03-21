8055a08c5f52b29d3c21178f3404004dfe8e7f23
paulaaortga <159927869+paulaaortga@users.noreply.github.com>
2024-03-21 17:05:19 +0100

    Update laberinto.py

--- laberinto.py
+++ laberinto.py
@@ -1,7 +1,30 @@
 
+import time
+
+class ThreadManager:
+    def __init__(self):
+        self.threads = []
+
+    def addThread(self, thread):
+        self.threads.append(thread)
+
+    def start(self):
+        for thread in self.threads:
+            thread.start()
+
+    def join(self):
+        for thread in self.threads:
+            thread.join()
+
+    def stop(self):
+        for thread in self.threads:
+            thread.stop()  
+
 class Game:
     def __init__(self):
         self.maze = None
+        self.bicho = []
+        self.threadManager = ThreadManager()
         
     def create_wall(self):
         return Wall()
@@ -47,6 +70,40 @@ class Game:
         room2.north = door
         return self.maze
 
+    def fabricarBichoAgresivo(self, room):
+        bicho = Bicho(Agresivo())
+        bicho.vidas = 5
+        bicho.poderes = 2
+        bicho.posicion = room
+        return bicho
+
+    def fabricarBichoPerezoso(self, room):
+        bicho = Bicho(Perezoso())
+        bicho.vidas = 3
+        bicho.poderes = 0
+        bicho.posicion = room
+        return bicho
+
+    def fabricarNorte(self):
+        return Norte().get_instance()
+
+    def fabricarEste(self):
+        return Este().get_instance()
+
+    def fabricarOeste(self):
+        return Oeste().get_instance()
+
+    def fabricarSur(self):
+        return Sur().get_instance()
+    
+    def lanzarHilos(self):
+        for bicho in self.bicho:
+            self.threadManager.addThread(bicho)
+        self.threadManager.start()
+        
+    def stopThreds(self):
+        self.threadManager.stop()
+        self.threadManager.join()
         
     
 class MapElement:
@@ -55,16 +112,6 @@ class MapElement:
     def entrar(self):
         pass
 
-class Maze(MapElement):
-    def __init__(self):
-        self.rooms = []
-
-    def addRoom(self, room):
-        self.rooms.append(room)
-
-    def entrar(self):
-        self.rooms[0].entrar()
-        
         
 class Hoja(MapElement):
     def accept(self, visitor):
@@ -83,7 +130,40 @@ class Contenedor(MapElement):
         
     def eliminarhijo(self, hijo):
         self.hijos.remove(hijo)
+        
+    def addOrientation(self, orientation):
+        self.orientations.append(orientation)
+    
+    def removeOrientation(self, orientation):
+        self.orientations.remove(orientation)
 
+    def goNorth(self, someone):
+        self.north.enter(someone)
+        
+    def goEast(self, someone):
+        self.east.enter(someone)
+        
+    def goSouth(self, someone):
+        self.south.enter(someone)
+        
+    def goWest(self, someone):
+        self.west.enter(someone)
+
+class Maze(Contenedor):
+    def __init__(self):
+        self.rooms = []
+
+    def addRoom(self, room):
+        self.rooms.append(room)
+
+    def entrar(self, alguien):
+        self.rooms[0].entrar(alguien)
+        
+    def getRoom(self, id):
+        for room in self.rooms:
+            if room.id == id:
+                return room
+        return None
 
 class Room(MapElement):
     def __init__(self, id):         
@@ -93,8 +173,8 @@ class Room(MapElement):
         self.south = Wall()
         self.id = id
 
-    def entrar (self):
-        print("Entrando en la habitación", self.id)
+    def entrar (self, alguien):
+        print(alguien, "entrando en la habitación", self.id)
 
         
         
@@ -116,6 +196,16 @@ class Wall(MapElement):
     def entrar(self):
         print("¡No puedes entrar aquí!")
 
+class Bomba (Decorator):
+    def __init__(self):
+        super().__init__()
+        self.active = False
+
+    def print(self):
+        print("Bomba")
+
+    def entrar(self, alguien):
+        print(alguien + " caminó hacia una bomba")
 
 
 class BombedGame(Game):
@@ -133,13 +223,33 @@ class BombedWall(Wall):
 
 class Bicho:
 
-    def __init__(self, vidas, poderes):
+    def __init__(self, vidas, poderes, posicion, modo):
         self.vidas = vidas
         self.poderes = poderes
+        self.posicion = None
+        self. modo = modo
 
     def actua(self):
         print("El bicho está actuando...")
-        self.usar_poder()
+        self.modo.actua(self)
+        
+    def esAgresivo(self):
+        return self.modo.esAgresivo()
+    
+    def esPerezoso(self):
+        return self.modo.esPerezoso()
+    
+    def irNorte(self):
+        self.posicion.irNorte(self)
+        
+    def irEste(self):
+        self.posicion.irEste(self)
+        
+    def irOeste(self):
+        self.posicion.irOeste(self) 
+        
+    def irSur(self):
+        self.posicion.irSur(self)
 
 class Modo:
 
@@ -148,17 +258,35 @@ class Modo:
 
     def actua(self, bicho):
         self.caminar(bicho)
+        self.dormir(bicho)
         
     def caminar(self, bicho):
         pass
+    
+    def dormir(self, bicho):
+        time.sleep(4)
+        print(bicho, "dormido")
+        
+    def esAgresivo(self):
+        return False
+    
+    def esPerezoso(self):
+        return False
         
 class Agresivo(Modo):
     def __init__(self):
         super().__init__("ataque")
+        
+    def esAgresivo(self):
+        return True
+    
 
 class Perezoso(Modo):
     def __init__(self):
         super().__init__("defensa")
+        
+    def esPerezoso(self):
+        return True
 
 class Orientacion: 
     
@@ -170,36 +298,45 @@ class Orientacion:
         
 class Norte(Orientacion):
     def __init__(self):
-        pass
+        if not Norte._instance:
+            super().__init__()
+            Norte._instance = self
     
     def poner(self):
         super().poner("norte")
 
 class Sur(Orientacion):
     def __init__(self):
-        pass
+        if not Sur._instance:
+            super().__init__()
+            Sur._instance = self
     
     def poner(self):
         super().poner("sur")
     
 class Este(Orientacion):
     def __init__(self):
-        pass
+        if not Este._instance:
+            super().__init__()
+            Este._instance = self
     
     def poner(self):
         super().poner("este")
     
 class Oeste(Orientacion):
     def __init__(self):
-        pass
+        if not Oeste._instance:
+            super().__init__()
+            Oeste._instance = self
     
     def poner(self):
         super().poner("oeste")
     
     
 game = Game()
+alguien = "Pepito"
 game.make2RoomsMaze()
-game.maze.entrar()
+game.maze.entrar(alguien)
 
 game= Game()
 game.make2RoomsMazeFM()
@@ -207,4 +344,5 @@ game.make2RoomsMazeFM()
 
 game = BombedGame()
 game.make2RoomsMazeFM()
-game.maze.entrar()
+game.maze.entrar(alguien)
+
